7c896f5d1cb277144ade02af215ee1440961ded0
Nahida Zakir <nahidazakir15@gmail.com>
2023-10-23 23:55:52 +0600

    Form Validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,50 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [count,setCount]=useState(0);
+  const [error,setError]=useState('');
 
-
-  
     const handleSubmit = (event) => {
       event.preventDefault();
-  
-    
+      
+      setCount(0);
       const email = event.target.Email.value;
       const password = event.target.Password.value;
+    
+        if(!/\S+@\S+\.\S+/.test(email)){
+          setError('Enter valid Email');
+          setCount(count+1);
+          return
+        }
+        if(password.length <1){
+          setError('Enter your Password');
+          setCount(count+1);
+          return
+        }
 
-      if(email.length<1){
-       
-      }
 
+        
         event.target.reset();
+        navigate('/home');
     };
     return (
         <div className='bg-cyan-100 h-screen p-9'>
+
             <div className=' place-content-center  max-w-[600px] mx-auto '>
                 <div className='bg-white h-full rounded-l-lg'> 
                     <h1 className='text-4xl font-bold text-cyan-500 mt-8 pt-10 mb-2'>Log in to Mycontacts</h1>
                     
-                    <p className='text-slate-400'>or use your email account</p>
-
-                    <div className='mt-8'>
-                    <form onSubmit={handleSubmit}>
+                    
+                    {
+        count === 0 ? <p></p> :   
+        
+        <div className="alert alert-error w-full max-w-xs mx-auto"><svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>{error}</span></div>
+      }
+                    <div className='mt-5'>
+                    <form  onSubmit={handleSubmit}>
       <input
         type="text"
         id="Email"
@@ -52,14 +68,15 @@ const Login = () => {
 
       <br />
       <br />
-<Link to='/home'>
-<button  className="btn btn-neutral " type="submit">Submit</button>
-</Link>
+
+<button   className="btn btn-neutral " type="submit">Submit</button>
+
     
 
       <br />
       <br />
-
+   
+    
     </form>   
 
                 </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,22 +1,99 @@
+import { Add, EmailSharp } from '@mui/icons-material';
+import axios from 'axios';
 import React, { useState }  from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route, useNavigate } from 'react-router-dom';
 const Signup = () => {
+
+
+
+
+
+
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
+    const [count,setCount]=useState(0);
+    const [error,setError]=useState('');
   
+
+
     const handleSubmit = (event) => {
+      
       event.preventDefault();
-  
+      setCount(0);
       const Firstname = event.target.FirstName.value;
       const Lastname = event.target.LastName.value;
       const Password  = event.target.Password.value;
-      const  Phonenumber = event.target.PhoneNumber.value;
+      const Retypepassword  = event.target.Retypepassword.value;
+      const  Primaryphonenumber = event.target.PrimaryphoneNumber.value;
+      const  Secondaryphonenumber = event.target.SecondaryphoneNumber.value;
       const Email  = event.target.Email.value;
       const Address  = event.target.Address.value;
       const Profession  = event.target.Profession.value;
       const Tagword  = event.target.TagWord.value;
       
-      setMessage(`Hello ${Firstname} ${Lastname} ${Password} ${Phonenumber} ${Email} ${Address} ${Profession} ${Tagword}!`);
-        event.target.reset();
+      if(Firstname.length <1){
+        setError('Enter your First Name');
+        setCount(count+1);
+        return
+      }
+      else if(Lastname.length <1){
+        setError('Enter your Last Name');
+        setCount(count+1);
+        return
+      }
+      else if(Primaryphonenumber.length <1){
+        setError('Enter your Phonenumber');
+        setCount(count+1);
+        return
+      }
+      else if(!/\S+@\S+\.\S+/.test(Email)){
+        setError('Enter valid Email');
+        setCount(count+1);
+        return
+      }
+      else if(Profession.length <1){
+        setError('Enter your Profession');
+        setCount(count+1);
+        return
+      }
+      else if(Tagword.length <1) {
+        setError('Enter your Tagword');
+        setCount(count+1);
+        return
+      }
+      else if(Password.length <1){
+        setError('Enter your Password');
+        setCount(count+1);
+        return
+      }
+      else if(!(Retypepassword === Password)){
+        setError('Passwords dont match');
+        setCount(count+1);
+        return
+      }
+
+      else{
+
+      }
+      axios.post('https://localhost:7106/api/users', {
+        Firstname: Firstname,
+        Lastname: Lastname,
+        Primaryphonenumber: Primaryphonenumber,
+        Secondaryphonenumber: Secondaryphonenumber,
+        Email:Email,
+        Address:Address,
+        Profession:Profession,
+        Tagword:Tagword
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+
+    
+
+      
+      event.target.reset();
+      navigate('/home');
     };
     return (
         <div>
@@ -26,6 +103,12 @@ const Signup = () => {
 
                 <div className='bg-white h-full rounded-r-lg'> 
                     <h1 className='text-4xl font-bold text-cyan-500 mt-8 mb-2 pt-5'>Create Account</h1>
+                      
+                    {
+        count === 0 ? <p></p> :   
+        
+        <div className="alert alert-error w-full max-w-xs mx-auto"><svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>{error}</span></div>
+      }
                     <div className='mt-8'>
                     <form onSubmit={handleSubmit}>
       <input
@@ -46,9 +129,17 @@ const Signup = () => {
       />
          <input
         type="text"
-        id="PhoneNumber"
-        name="PhoneNumber"
-        placeholder="Phone Number"
+        id="PrimaryphoneNumber"
+        name="PrimaryphoneNumber"
+        placeholder="Primary Phone Number"
+        className="input  mt-1 input-bordered input-info w-full max-w-xs"
+
+      />
+      <input
+        type="text"
+        id="SecondaryphoneNumber"
+        name="SecondaryphoneNumber"
+        placeholder="Secondary Phone Number"
         className="input  mt-1 input-bordered input-info w-full max-w-xs"
 
       />
@@ -85,18 +176,26 @@ const Signup = () => {
 
       />
                <input
-        type="text"
+        type="password"
         id="Password"
         name="Password"
         placeholder="Password"
         className="input  mt-1 input-bordered input-info w-full max-w-xs"
+/>
+        <input
+        type="password"
+        id="Retypepassword"
+        name="Retypepassword"
+        placeholder="Retype Password"
+        className="input  mt-1 input-bordered input-info w-full max-w-xs"
 
       />
+      
 
 
       <br />
       <br />
-<Link to='/home'>  <button  className="btn btn-neutral " type="submit">Submit</button></Link>
+<button  className="btn btn-neutral " type="submit">Submit</button>
      
 
       <br />
